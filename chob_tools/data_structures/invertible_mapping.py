276c93d039bc2527b9611da2f96f9ce3bbffb690
Cameron Helsby <cameron.helsby@live.com>
2024-10-03 22:48:27 +0100

    Update and rename invertible_dict.py to invertible_mapping.py

--- chob_tools/data_structures/invertible_mapping.py
+++ chob_tools/data_structures/invertible_mapping.py
@@ -1,22 +1,19 @@
 from __future__ import annotations
 
-from collections.abc import MutableMapping, Iterator
-from typing import Optional, TypeVar
+from collections import defaultdict
+from collections.abc import MutableMapping, Mapping, Iterator
+from typing import cast
 
 
-KT = TypeVar('KT')
-VT = TypeVar('VT')
-
-
-class InvertibleDict(MutableMapping[KT, VT]):
+class InvertibleDict[K, V](MutableMapping[K, V]):
     __slots__ = ('_forward', '_backward')
 
     def __init__(
         self,
-        forward: Optional[dict[KT, VT]] = None,
+        forward: Optional[dict[K, V]] = None,
         /,
         *,
-        _backward: Optional[dict[KT, VT]] = None,
+        _backward: Optional[dict[K, V]] = None,
     ) -> None:
         if forward is None:
             self._forward = {}
@@ -31,7 +28,7 @@ class InvertibleDict(MutableMapping[KT, VT]):
             }
             self._check_non_invertible()
 
-    def __getitem__(self, key) -> VT:
+    def __getitem__(self, key) -> V:
         return self._forward[key]
 
     def __setitem__(self, key, value) -> None:
@@ -51,14 +48,14 @@ class InvertibleDict(MutableMapping[KT, VT]):
         del self._forward[key]
         del self._backward[value]
 
-    def __iter__(self) -> Iterator[KT]:
+    def __iter__(self) -> Iterator[K]:
         return iter(self._forward)
 
     def __len__(self) -> int:
         return len(self._forward)
 
     @property
-    def inv(self) -> InvertibleDict[VT, KT]:
+    def inv(self) -> InvertibleDict[V, K]:
         return self.__class__(self._backward, _backward=self._forward)
 
     def _check_non_invertible(self) -> None:
@@ -70,3 +67,36 @@ class InvertibleDict(MutableMapping[KT, VT]):
                     'to maintain invertability.'
                 )
             seen.add(val)
+
+
+class InvertibleSetMapping[K, V: set](Mapping[K, V]):
+    __slots__ = ("_mapping", "_inverse")
+
+    def __init__(
+        self, mapping: Mapping[K, V], /, *, _inverse: Mapping[K, V] | None = None
+    ) -> None:
+        self._mapping = mapping
+        self._inverse = self._get_inverse(mapping) if _inverse is None else _inverse
+
+    def __getitem__(self, key: K) -> V:
+        return self._mapping[key]
+
+    def __iter__(self) -> Iterator[K]:
+        return iter(self._mapping)
+
+    def __len__(self) -> int:
+        return len(self._mapping)
+
+    @property
+    def inv(self) -> InvertibleSetMapping[K, V]:
+        return self.__class__(self._inverse, _inverse=self._mapping)
+
+    @staticmethod
+    def _get_inverse(mapping: Mapping[K, V]) -> defaultdict[K, V]:
+        inverse: defaultdict[K, V] = defaultdict(cast(Callable[[], V], set))
+        for key, values in mapping.items():
+            if not isinstance(values, set):
+                raise TypeError(f"Values must be of type set, got {type(values)}")
+            for value in values:
+                inverse[value].add(key)
+        return inverse
